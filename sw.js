e8e06889215aa679aad2c43c16f75d8df3cb1f62
BurnedFart <ryansharpful@gmail.com>
2026-02-03 21:25:46 -0800

    sfd

--- sw.js
+++ sw.js
@@ -32,8 +32,8 @@ Object.setPrototypeOf(self.Response, OriginalResponse);
 self.Response.prototype = OriginalResponse.prototype;
 
 // Bump to force cache refresh
-const VERSION = 'v55';
-const CACHE_NAME = 'scramjet-proxy-cache-v55';
+const VERSION = 'v56';
+const CACHE_NAME = 'scramjet-proxy-cache-v56';
 
 try {
     importScripts("./lib/scramjet/scramjet.all.js");
